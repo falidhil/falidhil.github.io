50dd95c2fe5ad733fde83c4f9b10be87676fc312
Fawwaz Aulia Fadhil <fawwazauliafadhil@gmail.com>
2021-01-29 16:43:53 +0700

    Fawwaz Aulia Fadhil

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 function printResults() {
-    let tanggal = document.querySelector('.getdata').value;
+    let date = document.querySelector('.getdata').value
     
-    // if (tanggal === null) {
-    //     tanggal = '2021-01-27';
-    // }
-
-    let DOB = tanggal; 
-    let millisecondsBetweenDOBAnd1970 = Date.parse(DOB);
+    let birth = date; 
+    let millisecondsBetweenDOBAnd1970 = Date.parse(birth);
     let millisecondsBetweenNowAnd1970 = Date.now();
     let ageInMilliseconds = millisecondsBetweenNowAnd1970 - millisecondsBetweenDOBAnd1970;
-
 
     let milliseconds = ageInMilliseconds;
     let second = 1000;
@@ -19,23 +14,25 @@ function printResults() {
     let month = day * 30;
     let year = day * 365;
 
-    let years = Math.round(milliseconds / year);
+    let years = Math.floor(milliseconds / year);
     let months = years * 12;
     let days = years * 365;
     let hours = Math.round(milliseconds / hour);
     let seconds = Math.round(milliseconds / second);
 
-    let message = "Age in Years : " + years +
+    if (years !== years || months !== months || days !== days || hours !== hours || seconds !== seconds) {
+        return 0;
+    }
+
+    let message = "You've been living for: <br><br> Age in Years : " + years +
         "</br>Age in Months : " + months +
         "</br>Age in Days : " + days +
         "</br>Age in Hours : " + hours +
-        "</br>Age in Seconds : " + seconds +
-        "</br>Age in Milliseconds : " + milliseconds;
+        "</br>Age in Seconds : " + seconds;
 
     document.getElementById('placeholder').innerHTML = message;
-    document.getElementById('placeholder').style.display = 'initial'
-    
-    return tanggal;
+
+    return date;
 }
 
 window.onload = printResults;
